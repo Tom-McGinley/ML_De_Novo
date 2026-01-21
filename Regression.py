4857fdf8893d43e61af1a572ea5b9ce0f5574d45
Tom McGinley <u7653227@anu.edu.au>
2026-01-21 12:01:31 +1100

    Draft class implementation of regression model

---
+++ Regression.py
@@ -0,0 +1,29 @@
+import numpy as np
+import math
+
+class Regression():
+    
+    def __init__(self, data):
+        self.data = data 
+
+    #Least Squares Part
+    B = np.array([])
+
+
+    """
+    Regression Blueprint:
+    Types of Regression
+    1. Linear Regression
+        - Simple Linear Regression
+        - Multiple Linear Regression
+    
+    Linear regression inputs:
+        X - Input vector X^T = (X_1, X_2, ..., X_p)
+
+    Coefficient Estimation:
+        - Least Squares
+        RSS(B) = Sum(i=1, N, ())
+
+    2. Polynomial Regression
+
+    """
