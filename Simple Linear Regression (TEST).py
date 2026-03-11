00d79d810d1e666b883a7d999f031a8c7582c6f7
Tom McGinley <u7653227@anu.edu.au>
2026-03-12 02:39:32 +1100

    Loss functions added to regression: l1 and l2 variants

--- Simple Linear Regression (TEST).py
+++ Simple Linear Regression (TEST).py
@@ -1,5 +1,7 @@
 #Linear Regression implementation
 import numpy as np
+import numpy.typing as npt
+import math
 
 def predict(alpha:float, beta:float, x_i:float):
     return beta*x_i + alpha
@@ -7,11 +9,38 @@ def predict(alpha:float, beta:float, x_i:float):
 def error(alpha:float, beta:float, x_i: float, y_i:float):
     return y_i - predict(alpha, beta, x_i)
 
-def sse(alpha:float, beta:float, x: np.array, y: np.array):
+def sse(alpha:float, beta:float, x:npt.ArrayLike, y:npt.ArrayLike):
     assert len(x) == len(y)
     sqr_errors = [error(alpha, beta, x_i, y_j)**2 for x_i, y_j in zip(x, y)]
     return sum(sqr_errors)
 
-def least_squares_fit(x: np.array, y: np.array):
+def least_squares_fit(x:npt.ArrayLike, y:npt.ArrayLike):
     return null
 
+#Loss functions measuring distance
+def l1_loss(y:npt.ArrayLike, y_pred:npt.ArrayLike) -> float:
+    """Sum of abs values of diff between predicted values and actual values."""
+    return sum([abs(y_i - y_pred_i)] for y_i, y_pred_i in zip(y,y_pred))
+
+def mae_loss(y:npt.ArrayLike, y_pred:npt.ArrayLike):
+    """Mean absolute error (MAE), average of l1 losses across n observations"""
+    assert len(y) == len(y_pred)
+    assert len(y) > 0
+    return 1/len(y) * l1_loss(y, y_pred)
+
+def l2_loss(y:npt.ArrayLike, y_pred:npt.ArrayLike) -> float:
+    """L2 loss, sum of squared difference between actual and predicted values"""
+    return sum([(y_i - y_pred_i)**2 for y_i, y_pred_i in zip(y, y_pred)])
+
+def mse_loss(y:npt.ArrayLike, y_pred:npt.ArrayLike) -> float:
+    """Mean Squared Error (MSE)"""
+    return 1/len(y) * l2_loss(y, y_pred)
+
+def rmse_loss(y:npt.ArrayLike, y_pred:npt.ArrayLike) -> float:
+    """Root mean squared error loss function"""
+    return math.sqrt(mse_loss(y, y_pred))
+
+#Testing
+pred = [34 + (-4.6*2.37)]
+label = [24]
+print(l2_loss(label, pred))
